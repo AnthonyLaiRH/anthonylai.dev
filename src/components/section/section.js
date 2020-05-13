ad455b5304e9a02fba7522e9705437f7f36ec023
AnthonyLaiRH <lai.anthony.rh@gmail.com>
2020-05-13 16:31:41 -0400

    Fixed lottie animations with proper state

--- src/components/section/section.js
+++ src/components/section/section.js
@@ -4,38 +4,60 @@ import style from "./section.module.css";
 
 import Lottie from 'react-lottie';
 
-function getLottieDimensions(){
-  const { innerWidth: width } = window;
-  
-  if(width <= 500){
-    return 275;
-  }
-  else{
-    return 350;
-  }
-}
-
 class Section extends Component {
-    render(){
-        const defaultOptions = {
-            loop: this.props.loop,
-            autoplay: true, 
-            animationData: this.props.animationData.default,
-            rendererSettings: {
-              preserveAspectRatio: 'xMidYMid slice'
-            }
-        };
-        return (
-            <div className={style.container}>
-                <Lottie style={{margin: "0, 3rem"}} options={ defaultOptions } height={ getLottieDimensions() } width={ getLottieDimensions() }/>
-                <div className={style.content}>
-                    <h1>{this.props.sectionTitle}</h1>
-                    <br></br>
-                    <p>{this.props.children}</p>
-                </div>
-            </div>
-        )
+  constructor (props){
+    super(props)
+    this.state = {
+      width: window.innerWidth,
+      lottieWidth: 0,
     };
+  }
+
+  componentDidMount() {
+    this.handleWindowSizeChange() // Set width
+    window.addEventListener('resize', this.handleWindowSizeChange)
+    
+  }
+  
+  // make sure to remove the listener
+  // when the component is not mounted anymore
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange)
+  }
+  
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth })
+    
+    if(this.state.width <= 500){
+      this.setState({lottieWidth: 275});
+    }
+    else{
+      this.setState({lottieWidth: 350});
+    }
+  }
+  
+  
+
+  render(){
+    const defaultOptions = {
+        loop: this.props.loop,
+        autoplay: true, 
+        animationData: this.props.animationData.default,
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
+    return (
+        <div className={style.container}>
+            <Lottie style={{margin: "0, 3rem"}} options={ defaultOptions } height={ this.state.lottieWidth } width={ this.state.lottieWidth }/>
+            <div className={style.content}>
+                <h1>{this.props.sectionTitle}</h1>
+                <br></br>
+                <p>{this.props.children}</p>
+            </div>
+        </div>
+    )
+  };
 };
 
 Section.propTypes = {
