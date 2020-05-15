3e109f7012d1b19c4c01f9471967d75fa0219431
AnthonyLaiRH <lai.anthony.rh@gmail.com>
2020-05-15 15:51:49 -0400

    notes section established

--- src/pages/notes.js
+++ src/pages/notes.js
@@ -4,14 +4,32 @@ import style from "../styles/notes.module.css"
 import SEO from "../components/seo";
 import Layout from "../components/layout/layout";
 
+import data from "../assets/json/notes_info.json";
+
 class NotesPage extends React.Component {
     render(){
-
         return(
             <Layout>
                 <SEO title="Notes" />
                 <div className={style.container}>
-                    <h1>Still in Development Sorry..</h1>
+                    <h1>Course Notes</h1>
+                    {data.map(course => {
+                        return (
+                            <div className={style.listing}>
+                              <h2>{course.name}</h2> 
+                              &nbsp;
+                
+                              <h4>{course.code}</h4>
+                              
+                              <p>{course.sem}</p>
+                              
+                              <br />
+                              <a className={style.link} href={course.url} target="__blank">Go to Notion Page ➡</a>
+                              <br />
+                
+                            </div>
+                        )
+                    })}
                 </div>
             </Layout>
         );
